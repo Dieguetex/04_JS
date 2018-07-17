05752325ab920171658ffc9b3215127342e8b89a
Dieguetex <Diegopmansilla@gmail.com>
2018-07-17 12:29:54 +0200

    JS Aplicado a la web

--- 06_Web/Hola_amigo/app.js
+++ 06_Web/Hola_amigo/app.js
@@ -1,17 +1,30 @@
 function app() {
 
-    oDomNombre = document.querySelector("#nombre")
-    oDomBtnSaludar = document.querySelector("#btnSaludar")
-    oDomBtnBorrar = document.querySelector("#btnBorrar")
+    // Defino los nodos del DOM que me interesan
+    let oDomNombre = document.querySelector("#nombre")
+    let oDomBtnSaludar = document.querySelector("#btnSaludar")
+    let oDomBtnBorrar = document.querySelector("#btnBorrar")
+    let oDomOutput = document.querySelector('#output')
 
+    // Defino los manejadores de eventos de algunos nodos
+    // Normalmente usamos click, input y change
     oDomNombre.addEventListener('input', leerDatos)
+    /* oDomNombre.addEventListener('change', detectarChange) */
     oDomBtnSaludar.addEventListener('click', saludar)
     oDomBtnBorrar.addEventListener('click', borrar)
 
     //domNombre.value = "Pepe"
     function leerDatos() {
+        console.log('Input detectado')
         console.log(oDomNombre.value)
+        oDomOutput.innerHTML = `Hola, <strong>${oDomNombre.value}</strong>`
+        
     }
+    
+    /* function detectarChange() {
+        console.log('Change detectado')
+        console.log(oDomNombre.value)
+    } */
 
     function saludar() {
         console.log('Hola Amigo')
---
+++ 06_Web/Hola_amigo/app_new.js
@@ -0,0 +1,11 @@
+function app() {
+    //      FORMA DE TRABAJAR:
+    // 1º   Deinir los nodos del DOM:
+    // De momento sabemos solamente con 'querySelector'
+    // Se guarda el objeto en una variable
+    let x = document.querySelector('#nombre')
+
+    //la principal funcion es definir manejadores de eventos
+}
+
+document.addEventListener('DOMContentLoaded', app)
