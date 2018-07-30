8d570fa87b43cabbdf0936d793e7ed7f82c9e011
Dieguetex <Diegopmansilla@gmail.com>
2018-07-30 13:59:43 +0200

    abc

---
+++ 13_WEB_Eventos/eventos_propios/app.js
@@ -0,0 +1,30 @@
+
+function main() {
+
+    let evEscribir =  new Event('escribir')
+
+    document.querySelector('#btnPulsar')
+        .addEventListener('click', pulsar)
+
+    document.querySelector('main')
+        .addEventListener('escribir', escribir)
+
+    /* document.querySelector('#output')
+        .addEventListener('escribir', escribir) */
+
+    function pulsar(oEv) {
+        console.dir(oEv.target)
+        oEv.target.dispatchEvent(new Event('escribir', {
+            'bubbles': true,
+        }))
+    }
+
+    function escribir(oEv) {
+        console.dir(oEv)
+        document.querySelector('#output').innerHTML = 
+            "He recibido un evento 'escribir'"
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', main)
